aea66b3afa19d84d8e537f8a41cfd5dd8674f574
June Vic Cadayona <jvcadz@gmail.com>
2024-01-13 20:56:05 +0800

    Add operator migration

--- app/Enums/UserAccountEnum.php
+++ app/Enums/UserAccountEnum.php
@@ -6,6 +6,7 @@ enum UserAccountEnum: string
 {
     case ADMIN = 'admin';
     case BUS_OPERATOR = 'bus-operator';
+    case BUS_COOPERATIVE = 'bus-cooperative';
     case DRIVER = 'driver';
     case JOBSEEKER = 'jobseeker';
 }
