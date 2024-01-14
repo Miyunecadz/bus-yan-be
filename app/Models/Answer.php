e4dad0621e3b8d123988e8f3b181de8fdd9820ad
Louie Jay Albarico <louiejay.albarico@gmail.com>
2024-01-15 05:07:12 +0800

    Add: Added Soft Delete Cascades and applied it to affected models

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -11,4 +11,9 @@ class Answer extends Model
     use HasFactory, SoftDeletes;
 
     protected $guarded = [];
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,18 +2,26 @@
 
 namespace App\Models;
 
+use Dyrynda\Database\Support\CascadeSoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Question extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, CascadeSoftDeletes;
+    
+    protected $cascadeDeletes = ['answer'];
 
     protected $guarded = [];
 
     public function job()
     {
         return $this->belongsTo(Job::class);
+    }
+
+    public function answer()
+    {
+        return $this->hasOne(Answer::class);
     }
 }
